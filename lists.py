7aca414e8bc60268b7ca0940b6bed82f48dee1b2
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-12 16:59:36 +0300

    first example

--- lists.py
+++ lists.py
@@ -1,3 +1,6 @@
 ## List
 # List is collection
 # List is also named Collection , Array
+friends = ["Abdullahi", 10, True, "Khadija", 11, False]
+print(f"My frinds are {friends}")
+print("My friends are" , friends)
