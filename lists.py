521109a409a1bbd85b85058a003c3ea4cd23a5d0
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-12 17:39:11 +0300

    Exercise 2

--- lists.py
+++ lists.py
@@ -14,3 +14,14 @@ print(friends[-1])
 
 friends[1] = "Hashim"
 print(friends)
+
+# Exercise 1
+# Write a list of school subject and print them
+
+
+# Exercise 2
+# Write days of the week
+
+# Print Index 3 and last index
+
+# change index 4 to the number of that day
